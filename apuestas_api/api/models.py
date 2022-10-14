43606e1eb6f43735d120640eb5262ac2a122ddb2
fabian <fabiancalderon049@gmail.com>
2022-10-14 11:49:39 -0500

    modelo

---
+++ apuestas_api/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Cuota(models.Model):
+    partido = models.CharField(max_length=100)
+    pronostico= models.CharField(max_length=100)
+    cuota = models.DecimalField(max_digits=3, decimal_places=2)
