1d9a015efa811f7033e817f81f09d9fd98bf409a
fabian <fabiancalderon049@gmail.com>
2022-11-02 00:00:31 -0500

    update3

--- apuestas_api/api/admin.py
+++ apuestas_api/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Cuota
+from .models import Cuota, Ayer
 # Register your models here.
 admin.site.register(Cuota)
+admin.site.register(Ayer)
--- apuestas_api/api/models.py
+++ apuestas_api/api/models.py
@@ -5,3 +5,9 @@ class Cuota(models.Model):
     partido = models.CharField(max_length=100)
     pronostico= models.CharField(max_length=100)
     cuota = models.DecimalField(max_digits=3, decimal_places=2)
+
+class Ayer(models.Model):
+    partido = models.CharField(max_length=100)
+    pronostico= models.CharField(max_length=100)
+    cuota = models.DecimalField(max_digits=3, decimal_places=2)
+    resultado = models.BooleanField()
